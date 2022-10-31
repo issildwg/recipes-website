156104e02fdab1d9225435e2801c53d8fb8dcb38
Issi Ludwig <issiludwig@gmail.com>
2022-10-31 15:08:05 +0000

    mapping out pages and page routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function () {       // this should return the home page
     return view('welcome');
+    /*  -display all recipes - alphabetically
+        -have menu?
+        -user profile
+        -search recipes
+        -sign up to email list??  
+    */
+});
+
+Route::redirect('/home', '/');            //redirects you back to home page if you type route /home into URL
+
+
+
+Route::get('/home/{name}', function ($name) {           //takes users to their profile home (so typing localhost/home/issi takes me back to my profile returns 'Welcome back to your homepage issi!')
+    return "Welcome back to your homepage $name!";
+    /* displays :
+        -user details
+        -user settings (to update details)
+        -recipes posted
+        -comments posted??
+        -saved recipes
+    */
+});
+
+
+Route::get('/recipe/{name}', function ($name) {           //takes users to specific recipes
+    return "$name";
+    /* displays :
+        -recipe details
+        -user who posted recipe
+        -comments on post 
+    */
 });
