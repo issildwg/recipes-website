5a6616673ac63648c2cf4fd42b1f3a71f79274b0
Issi Ludwig <issiludwig@gmail.com>
2023-01-04 13:49:23 +0000

    sorting out the dashboard a little

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,16 +19,12 @@
 
 @extends('layouts.basic')   <!-- going into layout folder and gets migrations -->
 
-@section('title', 'Dashboard')
+@section('title', 'Welcome!')
 
 @section('content')
-    <p>Users:</p>
-        <ul>
-            @foreach ($users as $user)
-                <li>{{$user->name}}</li> <!-- lists -->
-            @endforeach
+           <p>Please select from the above tabs to begin</p>
 
-        </ul>                  <!-- unordered list -->
+
         
 @endsection <!-- sandwiched like this since there is a lot of content -->
 
